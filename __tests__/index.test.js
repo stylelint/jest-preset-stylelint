9b6f73bc403d9cad4b940387e4d6bb95927780bb
Stephen Edgar <stephen@netweb.com.au>
2018-04-07 19:26:42 +1000

    test: use `it` keyword for Jest tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 "use strict"
 
 const preset = require("../jest-preset.json");
-test('it contains expected keys', () => {
+it('should contain expected keys', () => {
   expect(Object.keys(preset)).toMatchSnapshot();
 });
