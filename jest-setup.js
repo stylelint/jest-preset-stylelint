b2d6f28d8f2dac5f482d6db36c0e499b1519f6ad
Aleks Hudochenkov <aleks@hudochenkov.com>
2020-05-10 00:51:52 +0200

    Add end-of-line mock

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Mock should be before stylelint required. Even if it's required inside other modules
+jest.mock('stylelint/lib/utils/getOsEol', () => () => '\n');
+
 const getTestRule = require('./getTestRule');
 
 global.testRule = getTestRule();
